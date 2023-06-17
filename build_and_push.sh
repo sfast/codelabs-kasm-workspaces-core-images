bdb6a84ad36341a78f85feea98cf2613474297f3
Artak Vardanyan <artakvg@gmail.com>
2023-06-17 19:34:14 +0000

    test no ssl

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,12 +2,18 @@
 
 ORG_NAME=dooqod
 
-podman build \
-    -t ${ORG_NAME}/core-ubuntu:focal \
-    --build-arg START_PULSEAUDIO=1 \
-    --build-arg BASE_IMAGE="ubuntu:20.04" \
-    --build-arg START_XFCE4=1 \
-    --build-arg BG_IMG=bg_focal.png \
-    -f dockerfile-kasm-core-ubuntu .
+IMAGE_NAME=desktop-ubuntu-x86-64
+IMAGE_TAG=focal
+BASE_IMAGE=docker.io/dooqod/ospod-ubuntu-x86-64:focal
+DOCKERFILE=dockerfile-kasm-core-ubuntu
 
-# podman push ${ORG_NAME}/core-ubuntu:focal
+podman build --authfile="/home/admin/.docker/authfile.json" \
+    -t ${ORG_NAME}/${IMAGE_NAME}:${IMAGE_TAG} \
+    --build-arg BASE_IMAGE="${BASE_IMAGE}" \
+    --build-arg START_XFCE4=1 \
+    --build-arg START_PULSEAUDIO=1 \
+    --build-arg VNC_SSL_ONLY="" \
+    --build-arg BG_IMG=bg_focal.png \
+    -f ${DOCKERFILE} .
+
+# podman push ${ORG_NAME}/ospod-ubuntu-x86-64:focal
