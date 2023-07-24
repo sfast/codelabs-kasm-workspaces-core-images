64095d6dbc3f3fa216ddf04839cc222d044c4973
Artak Vardanyan <artakvg@gmail.com>
2023-07-24 19:48:12 +0000

    fix for ubuntu focal

--- src/ubuntu/install/terminal/install_terminal.sh
+++ src/ubuntu/install/terminal/install_terminal.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-cp /usr/share/applications/xfce4-terminal-emulator.desktop $HOME/Desktop
-chmod +x $HOME/Desktop/xfce4-terminal-emulator.desktop
-chown 1000:1000 $HOME/Desktop/xfce4-terminal-emulator.desktop
+terminal_desktop_path="xfce4-terminal-emulator.desktop"
+
+if [ -e "/usr/share/applications/xfce4-terminal-emulator.desktop" ]; then
+    terminal_desktop_path="xfce4-terminal-emulator.desktop"
+fi
+
+if [ -e "/usr/share/applications/exo-terminal-emulator.desktop" ]; then
+    terminal_desktop_path="exo-terminal-emulator.desktop"
+fi
+
+cp /usr/share/applications/$terminal_desktop_path $HOME/Desktop
+chmod +x $HOME/Desktop/$terminal_desktop_path
+chown 1000:1000 $HOME/Desktop/$terminal_desktop_path
